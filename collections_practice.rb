592352ef6d7e7545feb2a40698459794a98a61a5
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-12-02 20:21:29 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,7 +58,7 @@ def merge_data(arr1,arr2)
       arr2_name_hash.each do |name,details_hash|
         details_hash.each do |key,value|
           # binding.pry
-          if name = arr1_name_hash[i]
+          if name.include?(output[i])
           output[j][key] = value
         end
         end
